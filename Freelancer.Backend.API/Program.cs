f611ae2074493242980659638c875c56d8ac6bdd
Leckly <minejoyjoy@gmail.com>
2024-04-29 10:18:54 +0200

    Works!!! (added all services)

--- Freelancer.Backend.API/Program.cs
+++ Freelancer.Backend.API/Program.cs
@@ -1,10 +1,53 @@
+using Freelancer.Backend.API.ExceptionHandling;
 using Freelancer.Backend.CompositionRoot;
+using Freelancer.Backend.Domain.Entities;
+using Freelancer.Backend.Infrastructure;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("LocalDb");
 builder.Services.AddServices(connectionString ?? throw new ArgumentNullException());
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<int>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddRoles<IdentityRole<int>>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.ExpireTimeSpan = TimeSpan.FromDays(1);
+    options.SlidingExpiration = true;
+    options.LoginPath = null;
+    options.LogoutPath = null;
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = 401;
+        return Task.CompletedTask;
+    };
+});
+
+var policy = "_allowOrigin";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policy,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+        });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddProblemDetails();
@@ -15,6 +58,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
+app.UseCustomExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,6 +68,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy);
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
